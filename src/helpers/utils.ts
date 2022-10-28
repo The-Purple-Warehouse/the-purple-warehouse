8b536c6ca1ad47a3dd0a2755c0b9fed39b6f5b25
CMEONE <contact@kabirwearsato.ga>
2022-10-28 00:20:30 -0700

    Add API headers utility helper function

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,1 +1,5 @@
-
+export function addAPIHeaders(ctx: any) {
+    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    ctx.set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+}
