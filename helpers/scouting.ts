2cf3a3b2707e181dcacf73a83d55975e92a4e52b
CMEONE <contact@kabirwearsato.ga>
2023-03-04 23:48:56 -0800

    Fix helpers

--- helpers/scouting.ts
+++ helpers/scouting.ts
@@ -1,99 +1,113 @@
 import ScoutingEntry from "../models/scoutingEntry";
 import ScoutingCategory from "../models/scoutingCategory";
-import teams from "./teams";
+import { getTeamByNumber } from "./teams";
 import * as crypto from "crypto";
+import Resource from "../models/resource";
 
-export default class {
+export function getCategoryByIdentifier(identifier: string) {
+    return ScoutingCategory.findOne({ identifier });
+}
 
-    static async getCategoryByIdentifier(identifier: string) {
-        await ScoutingCategory.findOne({identifier});
-    }
-
-    static async getEntryByHash(hash: string) {
-        return await ScoutingEntry.findOne({hash});
-    }
-
-    static async entryExistsByHash(hash: string) {
-        return (await this.getEntryByHash(hash)) != null;
-    }
-
-    static async addEntry(contributingTeam: string, contributingUsername: string, match: number, team: string,
-                          color: string, data: [any], abilities: [any], counters: [any], timers: [any], ratings: [any],
-                          comments: string, timestamp: number) {
-        let stringified = JSON.stringify([
-            contributingTeam,
-            contributingUsername,
-            match,
-            team,
-            color,
-            data.map((dataObj) => ["category", dataObj.category, "data", dataObj.data]),
-            abilities.map((abilityObj) => ["category", abilityObj.category, "ability", abilityObj.ability]),
-            counters.map((counterObj) => ["category", counterObj.category, "counter", counterObj.counter]),
-            timers.map((timerObj) => ["category", timerObj.category, "timer", timerObj.timer]),
-            ratings.map((ratingObj) => ["category", ratingObj.category, "rating", ratingObj.rating]),
-            comments,
-            timestamp
-        ]);
-        let hash = crypto.createHash("sha256").update(stringified).digest("hex");
-        if (!(await this.entryExistsByHash(hash))) {
-            await (new ScoutingEntry({
-                contributor: {
-                    team: teams.getTeamByNumber(contributingTeam),
-                    username: contributingUsername
-                },
-                match: match,
-                team: team,
-                color: color,
-                data: data.map(dataObj => {
-                    return {
-                        category: this.getCategoryByIdentifier(dataObj.category),
-                        data: dataObj.data
-                    }
-                }),
-                abilities: abilities.map(abilityObj => {
-                    return {
-                        category: this.getCategoryByIdentifier(abilityObj.category),
-                        ability: abilityObj.ability
-                    }
-                }),
-                counters: counters.map(counterObj => {
-                    return {
-                        category: this.getCategoryByIdentifier(counterObj.category),
-                        counter: counterObj.counter
-                    }
-                }),
-                timers: timers.map(timerObj => {
-                    return {
-                        category: this.getCategoryByIdentifier(timerObj.category),
-                        timer: timerObj.timer
-                    }
-                }),
-                ratings: ratings.map(ratingObj => {
-                    return {
-                        category: this.getCategoryByIdentifier(ratingObj.category),
-                        rating: ratingObj.rating
-                    }
-                }),
-                clientTimestamp: timestamp,
-                serverTimestamp: (new Date()).getTime(),
-                hash: hash,
-                comments: comments
-            })).save();
-        }
-    }
-
-    static async addCategory(name: string, identifier: string, dataType?: string) {
+export async function addCategory(name: string, identifier: string, dataType?: string) {
+    let category = await getCategoryByIdentifier(identifier);
+    if(category == null) {
         if (dataType === undefined) {
-            await (new ScoutingCategory({
+            category = new ScoutingCategory({
                 name: name,
                 identifier: identifier
-            })).save();
+            });
         } else {
-            await (new ScoutingCategory({
+            category = new ScoutingCategory({
                 name: name,
                 identifier: identifier,
                 dataType: dataType
-            })).save();
+            });
         }
+        await category.save();
+        return category;
     }
+}
+
+export async function removeCategory(identifier: string) {
+    return (
+        (await ScoutingCategory.deleteOne({ identifier: identifier })).deletedCount > 0
+    );
+}
+
+export async function removeAllCategories() {
+    await ScoutingCategory.deleteMany({});
+}
+
+export function getEntryByHash(hash: string) {
+    return ScoutingEntry.findOne({hash});
+}
+
+export async function entryExistsByHash(hash: string) {
+    return (await getEntryByHash(hash)) != null;
+}
+
+export async function addEntry(contributingTeam: string, contributingUsername: string, match: number, team: string, color: string, data: [any], abilities: [any], counters: [any], timers: [any], ratings: [any], comments: string, timestamp: number) {
+    let stringified = JSON.stringify([
+        contributingTeam,
+        contributingUsername,
+        match,
+        team,
+        color,
+        data.map((dataObj) => ["category", dataObj.category, "data", dataObj.data]),
+        abilities.map((abilityObj) => ["category", abilityObj.category, "ability", abilityObj.ability]),
+        counters.map((counterObj) => ["category", counterObj.category, "counter", counterObj.counter]),
+        timers.map((timerObj) => ["category", timerObj.category, "timer", timerObj.timer]),
+        ratings.map((ratingObj) => ["category", ratingObj.category, "rating", ratingObj.rating]),
+        comments,
+        timestamp
+    ]);
+    let hash = crypto.createHash("sha256").update(stringified).digest("hex");
+    let entry = await getEntryByHash(hash);
+    if (entry == null) {
+        entry = new ScoutingEntry({
+            contributor: {
+                team: (await getTeamByNumber(contributingTeam))._id,
+                username: contributingUsername
+            },
+            match: match,
+            team: team,
+            color: color,
+            data: await Promise.all(data.map(async (element) => {
+                return {
+                    category: (await getCategoryByIdentifier(element.category))._id,
+                    data: element.data
+                }
+            })),
+            abilities: await Promise.all(abilities.map(async (element) => {
+                return {
+                    category: (await getCategoryByIdentifier(element.category))._id,
+                    ability: element.ability
+                }
+            })),
+            counters: await Promise.all(counters.map(async (element) => {
+                return {
+                    category: (await getCategoryByIdentifier(element.category))._id,
+                    counter: element.counter
+                }
+            })),
+            timers: await Promise.all(timers.map(async (element) => {
+                return {
+                    category: (await getCategoryByIdentifier(element.category))._id,
+                    timer: element.timer
+                }
+            })),
+            ratings: await Promise.all(ratings.map(async (element) => {
+                return {
+                    category: (await getCategoryByIdentifier(element.category))._id,
+                    rating: element.rating
+                }
+            })),
+            clientTimestamp: timestamp,
+            serverTimestamp: (new Date()).getTime(),
+            hash: hash,
+            comments: comments
+        });
+        await entry.save();
+    }
+    return entry;
 }
--- helpers/teams.ts
+++ helpers/teams.ts
@@ -1,9 +1,5 @@
 import Team from "../models/team";
 
-export default class {
-
-    static async getTeamByNumber(teamNumber: string) {
-        await Team.findOne({teamNumber});
-    }
-
+export function getTeamByNumber(teamNumber: string) {
+    return Team.findOne({ teamNumber });
 }
