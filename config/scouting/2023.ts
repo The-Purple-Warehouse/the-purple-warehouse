ceab5c7fc14f61d3cc83a8181ebd058e4acb58a8
CMEONE <contact@kabirwearsato.ga>
2023-03-11 00:47:18 -0800

    Fix bugs in CSV converter

--- config/scouting/2023.ts
+++ config/scouting/2023.ts
@@ -515,7 +515,7 @@ export function formatData(data, categories, teams) {
                         teams[entry.contributor.team] || 0
                     })`
                 ),
-                JSON.stringify((entry.comments || "").replace(/\n/g, "\\n"))
+                JSON.stringify((entry.comments || ""))
             ].join(",");
         })
         .join("\n")}`;
