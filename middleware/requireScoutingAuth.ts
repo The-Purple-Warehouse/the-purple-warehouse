a629f12cb1d43982fd60b7ddc321b70d42382790
CMEONE <contact@kabirwearsato.ga>
2023-03-11 15:36:16 -0800

    Add token-based authentication

--- middleware/requireScoutingAuth.ts
+++ middleware/requireScoutingAuth.ts
@@ -1,7 +1,44 @@
 import * as Koa from "koa";
+import { teamExistsByNumber } from "../helpers/teams";
+import auth from "../helpers/auth";
 
 export default async (ctx: Koa.Context, next: Koa.Next) => {
-    if (
+    const query = ctx.query as any;
+    if(query.token && query.team) {
+        if (!(await teamExistsByNumber(query.team))) {
+            ctx.body = {
+                success: false,
+                message:
+                    "Your team is not registered to use the scouting app. Please contact kabir@ramzan.me to register your team."
+            };
+        } else {
+            let res;
+
+            try {
+                res = await auth(
+                    query.team as string,
+                    "",
+                    query.token as string
+                );
+                if (!ctx.session) {
+                    ctx.body = {
+                        success: false,
+                        message: "There was an error logging in"
+                    };
+                } else {
+                    ctx.session.scoutingAuthed = true;
+                    ctx.session.scoutingTeamNumber = query.team as string;
+                    ctx.session.scoutingUsername = "";
+                    await next();
+                }
+            } catch (e) {
+                ctx.body = {
+                    success: false,
+                    message: e.message
+                };
+            }
+        }
+    } else if (
         !ctx.session ||
         !ctx.session.scoutingAuthed ||
         !ctx.session.scoutingTeamNumber ||
