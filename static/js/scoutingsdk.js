c5ff13ca71ed659754edabda6eb86a8920d502f2
CMEONE <CMEONE@users.noreply.github.com>
2023-03-12 01:00:26 +0000

    Format code

--- static/js/scoutingsdk.js
+++ static/js/scoutingsdk.js
@@ -1127,7 +1127,13 @@ ${_this.escape(teamNumber)} (Blue ${i + 1})
                                 .slice(1)
                                 .map((data) => {
                                     return `<tr>${data
-                                        .map((cell) => `<td>${cell.replaceAll("\\n", "<br>")}</td>`)
+                                        .map(
+                                            (cell) =>
+                                                `<td>${cell.replaceAll(
+                                                    "\\n",
+                                                    "<br>"
+                                                )}</td>`
+                                        )
                                         .join("")}</tr>`;
                                 })
                                 .join("");
@@ -1135,7 +1141,10 @@ ${_this.escape(teamNumber)} (Blue ${i + 1})
                             ".data-table > thead"
                         ).innerHTML = `<tr>${csv[0]
                             .map(
-                                (cell) => `<th>${cell.replaceAll('"', "").replaceAll("\\n", "<br>")}</th>`
+                                (cell) =>
+                                    `<th>${cell
+                                        .replaceAll('"', "")
+                                        .replaceAll("\\n", "<br>")}</th>`
                             )
                             .join("")}</tr>`;
                         element
