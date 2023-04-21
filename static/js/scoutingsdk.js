540186ab11db0f8f41c514f8c2aa5d622b022913
CMEONE <CMEONE@users.noreply.github.com>
2023-04-21 20:27:39 +0000

    Format code

--- static/js/scoutingsdk.js
+++ static/js/scoutingsdk.js
@@ -1516,7 +1516,13 @@ ${_this.escape(teamNumber)} (Blue ${i + 1})
                                                         return `<tr>${data
                                                             .map(
                                                                 (cell) =>
-                                                                    `<td${cell.includes(`<b>${teamNumber}</b>`) ? ` style="background-color: yellow;"` : ""}>${cell.replaceAll(
+                                                                    `<td${
+                                                                        cell.includes(
+                                                                            `<b>${teamNumber}</b>`
+                                                                        )
+                                                                            ? ` style="background-color: yellow;"`
+                                                                            : ""
+                                                                    }>${cell.replaceAll(
                                                                         "\\n",
                                                                         "<br>"
                                                                     )}</td>`
