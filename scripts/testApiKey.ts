720536edf67c17ae3dc2ae45e4a2e714f6a897f3
CMEONE <contact@kabirwearsato.ga>
2024-02-01 17:25:49 -0800

    Revert "Format code"
    This reverts commit 1dee51b0a2f8f4d43f945b59c74d8d7d16df10dc.

--- scripts/testApiKey.ts
+++ scripts/testApiKey.ts
@@ -21,24 +21,17 @@ async function testGenerateKey() {
     const keys = new Set<string>();
     for (let i = 0; i < 100; i++) {
         const key = generateAPIKey();
-        assert(
-            key != null && !keys.has(key),
-            "Generated key is null or already exists"
-        );
+        assert(key != null && !keys.has(key), "Generated key is null or already exists");
         keys.add(key);
     }
 }
 
 async function testKeys() {
-    const { key: unhashedKey, ...obj } = (await addAPIKey(keyOptions)) as any;
+    const { key: unhashedKey, ...obj } = await addAPIKey(keyOptions) as any;
     const properties = obj._doc;
     console.log(properties);
     assert(properties != null);
-    assert(
-        properties.apiKey != null &&
-            unhashedKey != null &&
-            properties.apiKey.length === 64
-    );
+    assert(properties.apiKey != null && unhashedKey != null && properties.apiKey.length === 64);
     assert(properties.name === keyOptions.name);
     assert(properties.team === keyOptions.team);
     assert(properties.app === keyOptions.app);
@@ -51,106 +44,36 @@ async function testKeys() {
     assert(properties.live);
     assert(properties.hashType === "sha256");
 
-    const disabledObj = (await disableAPIKey(unhashedKey)) as any;
+    const disabledObj = await disableAPIKey(unhashedKey) as any;
     assert(disabledObj != null, "Disabled key is null");
     assert(!disabledObj.live, "Disabled key is still live");
 
-    const enabledObj = (await enableAPIKey(unhashedKey)) as any;
+    const enabledObj = await enableAPIKey(unhashedKey) as any;
     assert(enabledObj != null, "Enabled key is null");
     assert(enabledObj.live, "Enabled key is not live");
 }
 
 async function testKeyVerification() {
-    const { key: unhashedKey, ...obj } = (await addAPIKey(keyOptions)) as any;
+    const { key: unhashedKey, ...obj } = await addAPIKey(keyOptions) as any;
     const properties = obj._doc;
 
     // verification passing in extra scopes
-    assert(
-        !(await verifyAPIKey(
-            unhashedKey,
-            properties.expiration,
-            properties.scouter,
-            properties.app,
-            properties.team,
-            properties.scopes.concat(["everything"])
-        )),
-        "Verification passed with extra scopes"
-    );
-    assert(
-        !(await verifyAPIKey(
-            unhashedKey,
-            properties.expiration,
-            properties.scouter,
-            properties.app,
-            properties.team,
-            properties.scopes.concat(["everything, read"])
-        )),
-        "Verification passed with 2 extra scopes"
-    );
+    assert(!(await verifyAPIKey(unhashedKey, properties.expiration, properties.scouter, properties.app, properties.team, properties.scopes.concat(["everything"]))), "Verification passed with extra scopes");
+    assert(!(await verifyAPIKey(unhashedKey, properties.expiration, properties.scouter, properties.app, properties.team, properties.scopes.concat(["everything, read"]))), "Verification passed with 2 extra scopes");
 
     // verification passing in given scopes or less
-    assert(
-        await verifyAPIKey(
-            unhashedKey,
-            properties.expiration,
-            properties.scouter,
-            properties.app,
-            properties.team,
-            properties.scopes
-        ),
-        "Verification failed with given scopes"
-    );
-    assert(
-        await verifyAPIKey(
-            unhashedKey,
-            properties.expiration,
-            properties.scouter,
-            properties.app,
-            properties.team,
-            [properties.scopes[0]]
-        ),
-        "Verification failed with 1 scope"
-    );
-    assert(
-        await verifyAPIKey(
-            unhashedKey,
-            properties.expiration,
-            properties.scouter,
-            properties.app,
-            properties.team,
-            [properties.scopes[1]]
-        ),
-        "Verification failed with 1 scope"
-    );
+    assert(await verifyAPIKey(unhashedKey, properties.expiration, properties.scouter, properties.app, properties.team, properties.scopes), "Verification failed with given scopes");
+    assert(await verifyAPIKey(unhashedKey, properties.expiration, properties.scouter, properties.app, properties.team, [properties.scopes[0]]), "Verification failed with 1 scope");
+    assert(await verifyAPIKey(unhashedKey, properties.expiration, properties.scouter, properties.app, properties.team, [properties.scopes[1]]), "Verification failed with 1 scope");
 
     // verification passing in no scopes
-    assert(
-        await verifyAPIKey(
-            unhashedKey,
-            properties.expiration,
-            properties.scouter,
-            properties.app,
-            properties.team,
-            []
-        ),
-        "Verification failed with no scopes"
-    );
+    assert(await verifyAPIKey(unhashedKey, properties.expiration, properties.scouter, properties.app, properties.team, []), "Verification failed with no scopes");
 
     // verification with not live key
-    const disabledObj = (await disableAPIKey(unhashedKey)) as any;
-    assert(
-        !(await verifyAPIKey(
-            unhashedKey,
-            properties.expiration,
-            properties.scouter,
-            properties.app,
-            properties.team,
-            properties.scopes
-        )),
-        "Verification passed with disabled key"
-    );
+    const disabledObj = await disableAPIKey(unhashedKey) as any;
+    assert(!(await verifyAPIKey(unhashedKey, properties.expiration, properties.scouter, properties.app, properties.team, properties.scopes)), "Verification passed with disabled key");
 
-    const enabledObj = (await enableAPIKey(unhashedKey)) as any;
+    const enabledObj = await enableAPIKey(unhashedKey) as any;
 }
 
 (async () => {
