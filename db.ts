b86300fd86e03ee8899595957122c73e9d316e1f
Daniel Gergov <93798575+25DanielG@users.noreply.github.com>
2025-01-05 13:31:32 -0800

    Update db.ts

--- db.ts
+++ db.ts
@@ -2,17 +2,13 @@ import mongoose from "mongoose";
 import config from "./config";
 
 if (config.db.username == "" && config.db.password == "") {
-    const mongoUrl = `mongodb://${config.db.host}:${config.db.port}/${config.db.database}`
-    console.log('mongoUrl:', mongoUrl)
     mongoose.connect(
-        mongoUrl,
+        `mongodb://${config.db.host}:${config.db.port}/${config.db.database}`,
         { useNewUrlParser: true, useUnifiedTopology: true }
     );
 } else {
-    const mongoUrl = `mongodb://${config.db.username}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.database}`
-    console.log('mongoUrl:', mongoUrl)
     mongoose.connect(
-        mongoUrl,
+        `mongodb://${config.db.username}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.database}`,
         { useNewUrlParser: true, useUnifiedTopology: true }
     );
 }
