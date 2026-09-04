68d7752dee88a9581fab122d87ffbb90594cee7d
Elaine Huang <huang.elaine.ca@gmail.com>
2026-09-03 21:30:14 -0700

    Revert "[prod deploy] spacebar increment & bug fixes"

--- static/js/scoutingsdk.js
+++ static/js/scoutingsdk.js
@@ -4833,13 +4833,13 @@ ${_this.escape(teamNumber)} (Blue ${i + 1})
                                 <div class="counter-plus">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>
                                 </div>
-                                <div class="counter-bulk" data-amount="3">+3</div>
-                                <div class="counter-bulk" data-amount="1">+1</div>
+                                <div class="counter-bulk" data-amount="10">+10</div>
+                                <div class="counter-bulk" data-amount="5">+5</div>
                                 <div class="counter-minus">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
                                 </div>
-                                <div class="counter-bulk" data-amount="-1">-1</div>
-                                <div class="counter-bulk" data-amount="-3">-3</div>
+                                <div class="counter-bulk" data-amount="-5">-5</div>
+                                <div class="counter-bulk" data-amount="-10">-10</div>
                                 <div class="counter-count">${initCount}</div>
                             </div>
                         </div>`;
@@ -4875,34 +4875,6 @@ ${_this.escape(teamNumber)} (Blue ${i + 1})
                                 );
                             }
                         };
-
-                        document.addEventListener("keydown", async (e) => {
-                            if (e.code === "Space") {
-                                if (
-                                    row
-                                        .closest(".preset")
-                                        ?.classList.contains("none")
-                                ) {
-                                    return;
-                                }
-
-                                if (opt.value !== "fsa") {
-                                    return;
-                                }
-
-                                e.stopPropagation();
-                                let cur = parseInt(ecount.innerText) || 0;
-                                if (opt.max !== undefined && cur >= opt.max) {
-                                    return;
-                                }
-                                values.push(opt.value);
-                                locations.push(location);
-                                dcounter++;
-                                ecount.innerText = cur + 1;
-                                tally();
-                                await hub();
-                            }
-                        });
 
                         plus.addEventListener("click", async (e) => {
                             e.stopPropagation();
